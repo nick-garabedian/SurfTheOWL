f0d49fdeb18e5b6d9e18ebfaf1f3165a5de26ac0
mannimaen <mani-we@web.de>
2021-03-24 18:10:45 +0100

    display all dependencies in browser (rough version). add easy way to set all up (start localserver, open browser an call localserver)

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # SurfTheOWL
-## requirements
+## Requirements
 + Python 3
 + package owlready2
 ```pip install owlready2```
 + package django ```pip install django```
+
+## How to strat SurfTheOWL
+Just open the "start SurfTheOWL.py" file. 
+This will open your Browser, and you can start surfing the OWL.
+It also opens a little GUI whit which you can terminate all started subprocesses.(big red Button) 
+
+
+## Surf another OWL 
+replace the "```TriboDaterFAIR_v0.4.owl```" with a new file, in dir ```./SurfTheOWL/TriboDaterFAIR_v0.4.owl```.
+Change the name of the to opened file in the script "SurfTheOWL.py", dir ```./SurfTheOWL/SurfTheOWL.py```
---
+++ start SurfTheOWL.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import webbrowser
+import subprocess
+import time
+import signal
+
+
+django_server = subprocess.Popen(['python','manage.py','runserver']) # start localserver
+
+ # open browser and call local server
+
+def terminate():
+    django_server.send_signal(signal.CTRL_C_EVENT) # quit local server
+    window.quit()  # quit tkinter window
+
+window = tk.Tk()
+window.title('SurfTheOWL')
+window.geometry('600x300')
+
+GUI_head = tk.Label(text='SurfTheOWL')
+GUI_info = tk.Text(window,  height=10, width=250)
+text = 'SurfTheOwl runs a Localserver in the background,\nto display the OWL dependencies in a Browser.\n\
+By terminating this window you also terminate the Localserver.'
+GUI_terminate = tk.Button(text='terminate SurfTheOWL', command=terminate, bg='red' )
+GUI_head.pack()
+GUI_head.config(font=('Arial', 30))
+GUI_info.pack()
+GUI_info.insert('end', text)
+GUI_info.configure(state='disabled')
+GUI_terminate.pack()
+GUI_terminate.config(font=('Arial', 26))
+webbrowser.open("http://127.0.0.1:8000/")
+window.mainloop()
