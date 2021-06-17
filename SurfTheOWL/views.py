9c78ad3f806aed7c02d62434ec9585ca44c7e808
mannimaen <mani-we@web.de>
2021-06-17 17:53:00 +0200

    implementing copy buttons in web browser via JavaScript

--- SurfTheOWL/views.py
+++ SurfTheOWL/views.py
@@ -35,12 +35,12 @@ def search(request): #search call of website
             global html_code
             if isinstance(complete_dict, dict):  # complete dict is dict and no list
                 for key in complete_dict.keys(): # for each key
-                    html_code += "<div class=" +just_odd_layer_seperation(depth) + "><hr class=\"limb_root\"><span class=\"bulletpoint\"> &#9660; </span><span class=\"layer"+ str(depth + 2) +"\"><b>" + key + " :</b></span>"
+                    html_code += "<div class=" +just_odd_layer_seperation(depth) + "><hr class=\"limb_root\"><span class=\"bulletpoint\"> &#9660; </span><span class=\"layer"+ str(depth + 2) +"\"><b>" + key + " </b></span><button onclick='copy_to_clipboard(\""+str(key)+"\")'><img src='https://img.icons8.com/ios/10/000000/copy.png'/></button><span><b>: </b></span>"
                     if isinstance(complete_dict[key], str) or isinstance(complete_dict[key], list): # if dict key(value) is no dict
                         if isinstance(complete_dict[key], list): # if value is list
                             html_code += "<div class=\"list\"><table>"#"<div class=\"list\">"
                             for element in complete_dict[key]: # for each element in list
-                                html_code += "<tr><td>" + str(element) + "</td></tr>" # insert each value
+                                html_code += "<tr><td><span class=\"bullet\"> &bull;  </span>" + str(element) + " <button onclick='copy_to_clipboard(\""+str(element)+"\")'><img src='https://img.icons8.com/ios/10/000000/copy.png'/></button></td></tr>" # insert each value
                             html_code += "</table></div></div>"
                         else: # if value is str
                             html_code += "<span class=\"layer" + str(depth + 2) + "_value\"> " + complete_dict[
@@ -50,7 +50,7 @@ def search(request): #search call of website
                         html_code += "<br>"
                         generate_html_form_dict_via_recusion(complete_dict[key], depth + 1) # call function again for child dict
                         html_code += "</div>"
-            elif isinstance(complete_dict, list):  # complete_dict is only a single list, in deep Objects possible
+            elif isinstance(complete_dict, list):  # if complete_dict is only a single list, in deep Objects possible
                 html_code += "<div class=\"list\"><table>"#"<div class=\"list\">"
                 for element in complete_dict:  # for each element in list
                     html_code += "<tr><td>" + element + "</td></tr>"  # insert each value
@@ -64,17 +64,17 @@ def search(request): #search call of website
                                                    'html_code': html_code,
                                                    })
 
-def download_search_result_json(request):
+def download_search_result_json(request): # function to serve a downloadable JSON to request
     global search_output
-    searched_class = next(iter(search_output[0]))
+    searched_class = next(iter(search_output[0]))  # beautify existing JSON by input the special objects and normal Objects separately
     downloadable_json = {searched_class:{"special_Objects":[]}}
     for i in range(len(search_output[1])):
         downloadable_json[searched_class]["special_Objects"].append({search_output[1][i][0]:search_output[1][i][1]})
-    downloadable_json[searched_class]["normal_objects"] = search_output[0][searched_class]
+    downloadable_json[searched_class]["normal_objects"] = search_output[0][searched_class] # end beautifying
 
-    json_file = json.dumps(downloadable_json, indent=2, sort_keys=True)
-    response = FileResponse(json_file, charset='utf-8')
-    response['Content-Disposition'] = 'attachment; filename=' + str(searched_class)+ '_json_file'
-    response['Content-Type'] = 'application/json'
+    json_file = json.dumps(downloadable_json, indent=2, sort_keys=True)  # make a json file
+    response = FileResponse(json_file, charset='utf-8')  # setup response as File
+    response['Content-Disposition'] = 'attachment; filename=' + str(searched_class)+ '_json_file' # name JSON file
+    response['Content-Type'] = 'application/json' # specific file type
 
     return response
